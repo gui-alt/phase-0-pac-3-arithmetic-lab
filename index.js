7d67f64f7969cabc1718b710b381204af32dd85f
gui-alt <gui.sampaioribeiro@icloud.com>
2021-10-20 12:55:16 +1100

    done with assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+let x = (4, 3)
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return  a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+let number = 1;
+function increment(a) {
+    return a + 1;
+}
+function decrement(a) {
+    return a - 1;
+}
+function makeInt(a) {
+    return parseInt(a, 10)
+}
+
+function preserveDecimal(a) {
+    return parseFloat(a)
+}
+
+
+
+
+
+
